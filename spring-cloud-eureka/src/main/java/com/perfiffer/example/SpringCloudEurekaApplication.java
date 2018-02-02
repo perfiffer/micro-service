8245c95937e1c442d92696ec2683fd104d3ce015
perfiffer <perfiffer@163.com>
2018-02-02 11:09:56 +0800

    eureka service example

---
+++ spring-cloud-eureka/src/main/java/com/perfiffer/example/SpringCloudEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.perfiffer.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+}
