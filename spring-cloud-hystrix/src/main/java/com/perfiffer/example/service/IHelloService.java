b74c98dea2295908698e8e135369deb3cd2dba85
perfiffer <perfiffer@163.com>
2018-02-16 14:47:11 +0800

    spring cloud hystrix demo

---
+++ spring-cloud-hystrix/src/main/java/com/perfiffer/example/service/IHelloService.java
@@ -0,0 +1,23 @@
+package com.perfiffer.example.service;
+
+import com.perfiffer.example.service.impl.HelloServiceImpl;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @ClassName:${CLASSNAME}
+ * @Description:
+ * @author: lurufeng
+ * @company: 柠檬科技
+ * @date: 2018/2/16
+ * @version: v2.7
+ */
+@FeignClient(value = "SPRING-CLOUD-DEMO", fallback = HelloServiceImpl.class)
+public interface IHelloService {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(String name);
+
+}
---
+++ spring-cloud-hystrix/src/main/java/com/perfiffer/example/service/impl/HelloServiceImpl.java
@@ -0,0 +1,22 @@
+package com.perfiffer.example.service.impl;
+
+import com.perfiffer.example.service.IHelloService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @ClassName:${CLASSNAME}
+ * @Description:
+ * @author: lurufeng
+ * @company: 柠檬科技
+ * @date: 2018/2/16
+ * @version: v2.7
+ */
+public class HelloServiceImpl implements IHelloService {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @Override
+    public String hello(String name) {
+        return "error";
+    }
+}
