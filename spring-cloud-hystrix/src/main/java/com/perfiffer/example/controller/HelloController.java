6ea683187b1bcd748a16352695743b2b4e3bc4b4
perfiffer <perfiffer@163.com>
2018-02-16 14:47:07 +0800

    spring cloud hystrix demo

---
+++ spring-cloud-hystrix/src/main/java/com/perfiffer/example/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.perfiffer.example.controller;
+
+import com.perfiffer.example.service.IHelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName:${CLASSNAME}
+ * @Description:
+ * @author: lurufeng
+ * @company: 柠檬科技
+ * @date: 2018/2/16
+ * @version: v2.7
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    IHelloService helloService;
+
+    public String hello(@RequestParam String name) {
+        return helloService.hello(name);
+    }
+
+}
