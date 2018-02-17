0249eb0976a8e0802e68baeed0e3d11c99dc49e9
perfiffer <perfiffer@163.com>
2018-02-17 16:09:37 +0800

    spring cloud hystrix demo

--- spring-cloud-hystrix/src/main/java/com/perfiffer/example/SpringCloudHystrixApplication.java
+++ spring-cloud-hystrix/src/main/java/com/perfiffer/example/SpringCloudHystrixApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 @EnableFeignClients
 public class SpringCloudHystrixApplication {
 
