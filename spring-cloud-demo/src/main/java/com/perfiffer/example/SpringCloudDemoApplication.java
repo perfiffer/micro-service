6193f73afb26c65b0c7b6a665a0ade31a803c290
perfiffer <perfiffer@163.com>
2018-02-27 22:13:38 +0800

    example code

--- spring-cloud-demo/src/main/java/com/perfiffer/example/SpringCloudDemoApplication.java
+++ spring-cloud-demo/src/main/java/com/perfiffer/example/SpringCloudDemoApplication.java
@@ -2,10 +2,13 @@ package com.perfiffer.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class SpringCloudDemoApplication {
 
 	public static void main(String[] args) {
